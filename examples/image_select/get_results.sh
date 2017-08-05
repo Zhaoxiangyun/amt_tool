2947c6fa8e41921b5a038175289706bfa9457851
Kaichun Mo <kmo@adobe.com>
2017-08-04 18:05:32 -0700

    add image_select

---
+++ examples/image_select/get_results.sh
@@ -0,0 +1,3 @@
+python get_results.py \
+  --hit_ids_file=examples/image_select/hit_ids.txt \
+  > examples/image_select/results.txt
---
+++ launch_image_select.sh
@@ -0,0 +1,5 @@
+python launch_hits.py \
+      --html_template=image_select.html \
+      --hit_properties_file=hit_properties/image_select.json \
+      --input_json_file=examples/image_select/example_input.txt \
+      --hit_ids_file=examples/image_select/hit_ids.txt
